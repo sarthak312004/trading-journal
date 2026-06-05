86183e9ea155698adc5c3c9df3855276ab29e21c
srthk31 <sarthak31122004m@gmail.com>
2026-06-05 20:49:08 +0530

    added basic ADD Trade and UPDATE Tarade functionality

--- script.js
+++ script.js
@@ -72,18 +72,56 @@ LEARNING OBJECTIVES:
 =========================================================
 */
 
-console.log("starting here");
 
-function information(name, age, winrate){
-   const person = {
-      personName : name,
-      personAge : age,
-      winRate : winrate
-   }
-   console.log("name of the person:",person.personName);
-   console.log("age of the person:",person.personAge);
-   console.log("Win rate:",person.winRate);
+// adding trades
+const trades = [];
 
+const addTrade = (tradeObj) => {
+  if (tradeObj) {
+    trades.push(tradeObj);
+    return "trade saved successfuly";
+  } else {
+    return "Add trade details !";
+  }
+};
+
+console.log(
+  addTrade({
+    stockName: "HDFC",
+    date:"5/6/26",
+    entryPrice: 1400,
+    stopLoss: 1300,
+    target: 1600,
+    entryReason: "momentum shift and breakout of trendline with good volume in prior direction",
+  }),
+);
+
+addTrade({
+    stockName: "SBI",
+    date:"1/6/26",
+    entryPrice: 700,
+    stopLoss: 650,
+    target: 800,
+    entryReason: "Multi year breakout, good volume, selling exhaustion",
+  })
+
+console.log(trades);
+
+// updating trades
+
+const updateTrade = (trade) => {
+   for(let i = 0; i<trades.length; i++){
+
+      if(trade.stockName == trades[i].stockName && trade.date == trades[i].date){
+         // console.log("trade found");
+         for(let [key,value] of Object.entries(trade)){
+            trades[i][key] = value
+         }
+         console.log("trade updated !");
+         console.log(trades[i]);
+      }
+   }  
 }
 
-information("sarthak mahamuni", 21, 65)
+updateTrade({stockName:"HDFC",date:"5/6/26", entryPrice:1500, target:1700})
+updateTrade({stockName:"SBI", date:"1/6/26", entryReason:"price is at oversold area, and selling exhaustion is visible, took a trade on bullish momentum with resistance breakout"})
