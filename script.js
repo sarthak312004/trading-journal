1bb25c2b19fe1ba4514abed19c15fe3d692af3b4
srthk31 <sarthak31122004m@gmail.com>
2026-06-07 20:19:19 +0530

    implement trade entry storage in array

--- script.js
+++ script.js
@@ -76,52 +76,57 @@ LEARNING OBJECTIVES:
 // adding trades
 const trades = [];
 
-const addTrade = (tradeObj) => {
-  if (tradeObj) {
-    trades.push(tradeObj);
-    return "trade saved successfuly";
-  } else {
-    return "Add trade details !";
-  }
-};
+const addTrade = () => {  
+   const tradeInfo = {
+      date : document.querySelector('#select-date').value,
+      asset : document.querySelector('#select-asset').value,
+      style : document.querySelector('#select-style').value,
+      timeFrame : document.querySelector('#select-TF').value,
+      assetName : document.querySelector('#select-asset-name').value,
+      direction : document.querySelector('#select-direction').value,
+      entryPrice : document.querySelector('#entry-price').value,
+      exitPrice : document.querySelector('#exit-price').value,
+      quantity : document.querySelector('#enter-qnt').value,
+      context : document.querySelector('#add-context').value,
+      screenshot : document.querySelector('#file-upload').files[0]
+   }
+   // console.log(tradeInfo);
 
-console.log(
-  addTrade({
-    stockName: "HDFC",
-    date:"5/6/26",
-    entryPrice: 1400,
-    stopLoss: 1300,
-    target: 1600,
-    entryReason: "momentum shift and breakout of trendline with good volume in prior direction",
-  }),
-);
+   trades.push(tradeInfo)
+   console.log(trades);
+   
+   alert("Trade added successfully")
+   
+}
 
-addTrade({
-    stockName: "SBI",
-    date:"1/6/26",
-    entryPrice: 700,
-    stopLoss: 650,
-    target: 800,
-    entryReason: "Multi year breakout, good volume, selling exhaustion",
-  })
 
-console.log(trades);
+
+
+
+
+
+
+
+
+
+
+
 
 // updating trades
 
-const updateTrade = (trade) => {
-   for(let i = 0; i<trades.length; i++){
+// const updateTrade = (trade) => {
+//    for(let i = 0; i<trades.length; i++){
 
-      if(trade.stockName == trades[i].stockName && trade.date == trades[i].date){
-         // console.log("trade found");
-         for(let [key,value] of Object.entries(trade)){
-            trades[i][key] = value
-         }
-         console.log("trade updated !");
-         console.log(trades[i]);
-      }
-   }  
-}
+//       if(trade.stockName == trades[i].stockName && trade.date == trades[i].date){
+//          // console.log("trade found");
+//          for(let [key,value] of Object.entries(trade)){
+//             trades[i][key] = value
+//          }
+//          console.log("trade updated !");
+//          console.log(trades[i]);
+//       }
+//    }  
+// }
 
-updateTrade({stockName:"HDFC",date:"5/6/26", entryPrice:1500, target:1700})
-updateTrade({stockName:"SBI", date:"1/6/26", entryReason:"price is at oversold area, and selling exhaustion is visible, took a trade on bullish momentum with resistance breakout"})
+// updateTrade({stockName:"HDFC",date:"5/6/26", entryPrice:1500, target:1700})
+// updateTrade({stockName:"SBI", date:"1/6/26", entryReason:"price is at oversold area, and selling exhaustion is visible, took a trade on bullish momentum with resistance breakout"})
