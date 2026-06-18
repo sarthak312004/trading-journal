4be2c934637933c11587176d54f22f82f34a2354
srthk31 <sarthak31122004m@gmail.com>
2026-06-18 21:00:47 +0530

    fix: update with empty fields bug

--- script.js
+++ script.js
@@ -141,6 +141,14 @@ const trades = [
     screenshot: "",
   },
 ];
+let impInputs = [
+  "date",
+  "assetName",
+  "direction",
+  "entryPrice",
+  "exitPrice",
+  "quantity",
+];
 
 const addTrade = () => {
   const tradeInfo = {
@@ -167,15 +175,8 @@ const addTrade = () => {
   }
 
   // >>>>>>>>input validation
-  const impInputs = [
-    "date",
-    "assetName",
-    "direction",
-    "entryPrice",
-    "exitPrice",
-    "quantity",
-  ];
-  let emptyInputs = [];
+
+  const emptyInputs = [];
   if (
     !(
       tradeInfo.date &&
@@ -203,7 +204,7 @@ const addTrade = () => {
 };
 
 // -----------------display all existing trades
-let displayCount = 0
+let displayCount = 0;
 function display() {
   for (let obj of trades) {
     for (let [key, value] of Object.entries(obj)) {
@@ -220,15 +221,15 @@ function display() {
     const arrOfChild = Array.from(childrens);
     for (let node of arrOfChild) {
       node.remove();
-    } 
+    }
 
-     displayCount++
-    if(displayCount == 1 ){
-      const analyseBtn = document.createElement('button')
-      analyseBtn.className = "after-display-analyse-btn"
-      analyseBtn.innerText = "Analyse"
-      analyseBtn.onclick = () => analyse()
-      document.querySelector('.analytics-tab').appendChild(analyseBtn)
+    displayCount++;
+    if (displayCount == 1) {
+      const analyseBtn = document.createElement("button");
+      analyseBtn.className = "after-display-analyse-btn";
+      analyseBtn.innerText = "Analyse";
+      analyseBtn.onclick = () => analyse();
+      document.querySelector(".analytics-tab").appendChild(analyseBtn);
     }
     trades.forEach((tradeObj, index) => {
       const tradeCard = document.createElement("div");
@@ -460,7 +461,7 @@ const deleteTrade = (date, id) => {
 
 // ------------------------Update existing trades
 const showOnForm = (tradeObj) => {
-  console.log(tradeObj);
+  // console.log(tradeObj);
   document.querySelector("form h3").innerText = "/ Edit Your Trade";
 
   document.querySelector("#select-date").value = tradeObj.date;
@@ -475,7 +476,7 @@ const showOnForm = (tradeObj) => {
   document.querySelector("#add-context").value = tradeObj.context;
   document.querySelector("#file-upload").files[0];
 
-  const updateBtn = document.querySelector(".row-6 input");
+  const updateBtn = document.querySelector("#addtrade");
   updateBtn.value = "Update";
   updateBtn.onclick = () => updateTrade(tradeObj.id);
 };
@@ -496,31 +497,52 @@ const updateTrade = (tradeId) => {
     screenshot: document.querySelector("#file-upload").files[0],
   };
   console.log(trade);
+  console.log(impInputs);
 
-  let isExist = false;
-  if (trade.id) {
-    for (let obj of trades) {
-      if (obj.id == trade.id) {
-        isExist = true;
-        for (let [key, value] of Object.entries(trade)) {
-          obj[key] = value;
-        }
+  //find the existing trade in original trade array
+  const emptyInputs = [];
+  if (
+    !(
+      trade.date &&
+      trade.assetName &&
+      trade.direction &&
+      trade.entryPrice &&
+      trade.exitPrice &&
+      trade.quantity
+    )
+  ) {
+    for (let inputKey of impInputs) {
+      if (!trade[inputKey]) {
+        emptyInputs.push(inputKey);
       }
     }
+    alert(`Enter :${emptyInputs}`);
   } else {
-    console.log("Enter trade id");
-  }
-  if (isExist) {
-    // console.log("Trade updated successfully !");
-    alert("Trade updated successfully !");
-    document.querySelector("form").reset();
+    let isExist = false;
+    if (trade.id) {
+      for (let obj of trades) {
+        if (obj.id == trade.id) {
+          isExist = true;
+          for (let [key, value] of Object.entries(trade)) {
+            obj[key] = value;
+          }
+        }
+      }
+    } else {
+      console.log("Enter trade id");
+    }
+    if (isExist) {
+      // console.log("Trade updated successfully !");
+      alert("Trade updated successfully !");
+      document.querySelector("form").reset();
 
-    const addBtn = document.querySelector(".row-6 input");
-    addBtn.value = "Add Trade";
-    addBtn.onclick = () => addTrade();
-  } else {
-    // console.log("Given Id does not exist !",trade);
-    alert("Given Id does not exist !");
+      const addBtn = document.querySelector(".row-6 input");
+      addBtn.value = "Add Trade";
+      addBtn.onclick = () => addTrade();
+    } else {
+      // console.log("Given Id does not exist !",trade);
+      alert("Given Id does not exist !");
+    }
   }
 };
 // updateTrade({ id:1, assetName:"Mahindra Motors", asset:"Equity", entryPrice:1780, exitPrice:2000 });
@@ -531,9 +553,9 @@ const analyse = () => {
   for (let value of nodes) {
     value.remove();
   }
-    const lastNode = document.querySelector(".analytics-tab").children
-    if(lastNode.length == 3){
-    lastNode[2].remove()
+  const lastNode = document.querySelector(".analytics-tab").children;
+  if (lastNode.length == 3) {
+    lastNode[2].remove();
   }
 
   if (trades.length === 0) {
@@ -545,17 +567,16 @@ const analyse = () => {
     document.querySelector(".analytics-info").appendChild(para2);
   } else {
     // ----------NET PNL
-    displayCount = 0
+    displayCount = 0;
     let totalTrades = 0;
-    
-
 
     const netPnl = trades.reduce((acc, trade) => {
       totalTrades++;
       const entry = Number(trade.entryPrice);
       const exit = Number(trade.exitPrice);
       const qty = Number(trade.quantity);
-      let pnl = trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
+      let pnl =
+        trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
       return acc + pnl;
     }, 0);
     // console.log(netPnl);
@@ -574,50 +595,51 @@ const analyse = () => {
         trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
       if (pnl > 0) {
         wins++;
-        totalOfWin += pnl 
+        totalOfWin += pnl;
       } else {
         losses++;
-        totalOfLoss += pnl
+        totalOfLoss += pnl;
       }
     });
-    const winRate = (wins / totalTrades) * 100
+    const winRate = Math.floor((wins / totalTrades) * 100);
     // console.log(winRate);
-    const avgWin = Math.trunc(totalOfWin / wins)
-    const avgLoss = Math.trunc(totalOfLoss / losses)
-    
-    
-    // --------BEST & WORST TRADE 
-    let profitableAsset = ""
-    let lossMakingAsset = ""
+    const avgWin = Math.trunc(totalOfWin / wins);
+    const avgLoss = Math.trunc(totalOfLoss / losses);
+
+    // --------BEST & WORST TRADE
+    let profitableAsset = "";
+    let lossMakingAsset = "";
     const bestTrade = trades.reduce((acc, trade) => {
       const entry = Number(trade.entryPrice);
       const exit = Number(trade.exitPrice);
       const qty = Number(trade.quantity);
-      let pnl = trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
-      if(pnl > acc){
-        acc = pnl
-        profitableAsset = trade.asset
-        return acc
-      }else{
-        return acc
+      let pnl =
+        trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
+      if (pnl > acc) {
+        acc = pnl;
+        profitableAsset = trade.asset;
+        return acc;
+      } else {
+        return acc;
       }
-    },0)
+    }, 0);
     // console.log(profitableAsset);
 
     const worstTrade = trades.reduce((acc, trade) => {
       const entry = Number(trade.entryPrice);
       const exit = Number(trade.exitPrice);
       const qty = Number(trade.quantity);
-      let pnl = trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
-      if(pnl < acc){
-        acc = pnl
-        lossMakingAsset = trade.asset
+      let pnl =
+        trade.direction == "Long" ? (exit - entry) * qty : (entry - exit) * qty;
+      if (pnl < acc) {
+        acc = pnl;
+        lossMakingAsset = trade.asset;
         // console.log(pnl);
-        return acc
-      }else{
-        return acc
+        return acc;
+      } else {
+        return acc;
       }
-    },0)
+    }, 0);
     // console.log(lossMakingAsset);
 
     const heading = document.createElement("p");
@@ -630,47 +652,46 @@ const analyse = () => {
 
     const PNL = document.createElement("p");
     PNL.className = "p&l";
-    PNL.style.margin = "2px"
+    PNL.style.margin = "2px";
     if (netPnl > 0) {
       PNL.style.color = "#00dd00";
       PNL.innerText = `Net P/L : +${netPnl}`;
     } else if (netPnl < 0) {
       PNL.style.color = "#dd6300";
-      PNL.innerText = `Net P/L : -${netPnl}`;
+      PNL.innerText = `Net P/L : ${netPnl}`;
     } else {
       PNL.innerText = `Net P/L : ${netPnl}`;
     }
     document.querySelector(".stat-card").appendChild(PNL);
 
     const tradeCount = document.createElement("p");
-    tradeCount.style.margin = "2px"
+    tradeCount.style.margin = "2px";
     tradeCount.innerText = `Total Trades : ${totalTrades}`;
     document.querySelector(".stat-card").appendChild(tradeCount);
 
     const totalWinrate = document.createElement("p");
-    totalWinrate.style.margin = "2px"
+    totalWinrate.style.margin = "2px";
     totalWinrate.innerText = `Win Rate : ${winRate}%`;
     document.querySelector(".stat-card").appendChild(totalWinrate);
 
     const bestAsset = document.createElement("p");
-    bestAsset.style.margin = "2px"
+    bestAsset.style.margin = "2px";
     bestAsset.innerText = `Best Performing Asset : ${profitableAsset}`;
     document.querySelector(".stat-card").appendChild(bestAsset);
 
     const poorAsset = document.createElement("p");
-    poorAsset.style.margin = "2px"
+    poorAsset.style.margin = "2px";
     poorAsset.innerText = `Poor Performing Asset : ${lossMakingAsset}`;
     document.querySelector(".stat-card").appendChild(poorAsset);
 
     const avgwin = document.createElement("p");
-    avgwin.style.margin = "2px"
+    avgwin.style.margin = "2px";
     avgwin.innerText = `Avg Win : ${avgWin}`;
     document.querySelector(".stat-card").appendChild(avgwin);
 
     const avgloss = document.createElement("p");
-    avgloss.style.margin = "2px"
+    avgloss.style.margin = "2px";
     avgloss.innerText = `Avg loss : ${avgLoss}`;
     document.querySelector(".stat-card").appendChild(avgloss);
-
   }
 };
